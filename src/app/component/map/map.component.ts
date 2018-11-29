d0daf2386e95ba3a3baf0625c54344addcd7461b
Freya Gaynor <dev@webwitch.org>
2018-11-29 12:30:41 -0600

    Attempting to make asynchronously-appearing markers.

--- src/app/component/map/map.component.ts
+++ src/app/component/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { FlatCoordinates } from 'src/app/models/flat-coordinates.model';
 
 @Component({
   selector: 'app-map',
@@ -7,8 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
   title = 'AGM Project';
-
-  public isuCoords = {
+  public coordinates$: FlatCoordinates[];
+  public isuCoords: FlatCoordinates = {
     latitude: 42.0149975,
     longitude: -93.6457346
   };
@@ -16,6 +18,20 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getCoordinates() {
+    const coordinateMockArr: FlatCoordinates[] = [
+      { latitude: 42.0149975, longitude: -93.6457346 },
+      { latitude: 42.0149975, longitude: -93.6457346 },
+      { latitude: 42.0149975, longitude: -93.6457346 },
+      { latitude: 42.0149975, longitude: -93.6457346 },
+      { latitude: 42.0149975, longitude: -93.6457346 }
+    ];
+
+    of(coordinateMockArr).subscribe(coords => {
+      this.coordinates$ = coords;
+    });
   }
 
 }
---
+++ src/app/models/flat-coordinates.model.ts
@@ -0,0 +1,7 @@
+/**
+ * Coordinates with just a latitude and longitude, e.g. 2d.
+ */
+export class FlatCoordinates {
+  latitude: number;
+  longitude: number;
+}
