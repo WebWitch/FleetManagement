8f05a6bc735cc7b916c36a41f76db38751292bfa
Freya Gaynor <dev@webwitch.org>
2018-12-03 13:29:39 -0600

    Fixed environment style.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,11 +6,11 @@ export const environment = {
     agm: 'AIzaSyAg8rOLrM2S_AZljmBoOtfAGLK8UTRpY0c'
   },
   firebase: {
-    apiKey: "AIzaSyBwTrUeeD0BlGs0rQXaxhuhr3VZwtHeZGA",
-    authDomain: "se339-fleet-management.firebaseapp.com",
-    databaseURL: "https://se339-fleet-management.firebaseio.com",
-    projectId: "se339-fleet-management",
-    storageBucket: "se339-fleet-management.appspot.com",
-    messagingSenderId: "1025479934830"
+    apiKey: 'AIzaSyBwTrUeeD0BlGs0rQXaxhuhr3VZwtHeZGA',
+    authDomain: 'se339-fleet-management.firebaseapp.com',
+    databaseURL: 'https://se339-fleet-management.firebaseio.com',
+    projectId: 'se339-fleet-management',
+    storageBucket: 'se339-fleet-management.appspot.com',
+    messagingSenderId: '1025479934830'
   }
 };
